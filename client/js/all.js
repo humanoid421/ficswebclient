d99e282c74d0991a871eabd9761e91cd63af4478
humanoid421 <brittonf@gmail.com>
2017-06-11 19:16:08 -0400

    drag and drop pieces work

--- client/js/all.js
+++ client/js/all.js
@@ -16,49 +16,48 @@ class Game {
     }
 }
 
-var INIT_S12 = {
-    w_name: 'whiteguy',
-    b_name: 'blackguy',
+var INIT_S12 = new Map();
+INIT_S12.set('43', {
+    w_name: 'steinitz',
+    b_name: 'burden',
     dur: 5,
     inc: 0,
     game_num: '43',
     w_clock: 500,
-    b_clock: 600,
-};
+    b_clock: 600, });
+INIT_S12.set('66', {
+    w_name: 'nakamura',
+    b_name: 'MVL',
+    dur: 5,
+    inc: 0,
+    game_num: '66',
+    w_clock: 500,
+    b_clock: 600, });
 
-var INIT_MOVES = 
-[
-'e4',
-'d6',
-'d4',
-'e6',
-'Nf3',
-'h6',
-'Bd3',
-'g6',
-];
+var INIT_MOVES = new Map();
+INIT_MOVES.set('43', ['e4', 'e5', 'Nf3', 'Nc6', 'Bc4', 'Nf6', 'Ng5', 'd5', 'exd5', 'Na5', 'd3', 'h6', 'Nf3', 'e4', 'Qe2', 'Nxc4', 'dxc4', 'Bg4', 'h3', 'Bh5', 'g4', 'Bg6', 'Nd4', 'h5', 'g5', 'Nd7', 'h4', 'Bc5', 'Be3', 'Ne5', 'Nd2', 'O-O', 'O-O-O', 'a5', 'N2b3', 'Ba7', 'a4', 'f5', 'gxf6', 'Rxf6', 'Bg5', 'Ng4', 'Ne6', 'Rxf2', 'Qxf2', 'Qxg5+', 'hxg5', 'Nxf2', 'Nxc7', 'Rc8', 'Nb5', 'Be3+', 'Kb1', 'Bxg5', 'Rhg1', 'Nxd1', 'Rxg5', 'h4', 'Rxg6', 'h3', 'Rg3', 'Nf2', 'Rxh3', 'Nxh3', 'Nd6', 'e3', 'Kc1', 'Rf8', 'Kd1', 'Ng1', ]);
+INIT_MOVES.set('66', ['e4', 'c5', 'Nf3', 'd6', 'd4', 'cxd4', 'Nxd4', 'Nf6', 'Nc3', 'a6', 'Bd3', 'e5', 'Nde2', 'Be7', 'O-O', 'O-O', 'Ng3', 'Be6', 'Nd5', 'Bxd5', 'exd5', 'g6', 'c4', 'Ne8', 'Bh6', 'Ng7', 'b4', 'Nd7', 'Rc1', 'a5', 'a3', 'axb4', 'axb4', 'Ra3', 'Ne4', 'f5', 'Nc3', 'e4', 'Be2', 'Bg5', 'Bxg5', 'Qxg5', 'c5', 'Ne5', 'c6', 'Nh5', 'Bxh5', 'gxh5', 'Kh1', 'Qh4', 'Qd4', 'Ng4', 'h3', 'f4', 'Kg1', 'e3', 'hxg4', 'hxg4', 'cxb7', 'exf2+', 'Rxf2', 'g3', 'Rxf4', 'Qh2+', 'Kf1', ]);
 
 
 var gamemap = new Map();
 
 
 function INIT() {
-    var game_num = '43';
-    gamemap.set(game_num, new Game(INIT_S12));
+    ['43','66'].forEach(game_num => {
+        gamemap.set(game_num, new Game(INIT_S12.get(game_num)));
 
+        var game = gamemap.get(game_num);
 
-    var game = gamemap.get(game_num);
+        var moves = INIT_MOVES.get(game_num);
 
-    var moves = INIT_MOVES;
-
-    for (var i=0; i<moves.length; i++) {
-        game.chess.move(moves[i]);
-        //game.fens[i] = game.chess.fen();
-        game.fens[i] = game.chess.fen().split(' ')[0];
-        //game.movetimes[i] = movetimes[i];
-    }
-    renderGame(game_num);
-    renderMoveList(game_num);
+        for (var i=0; i<moves.length; i++) {
+            game.chess.move(moves[i],{sloppy:true});
+            game.fens[i] = game.chess.fen().split(' ')[0];
+            //game.movetimes[i] = movetimes[i];
+        }
+        renderGame(game_num);
+        renderMoveList(game_num);
+    });
 
 }
 
@@ -69,7 +68,7 @@ function APPEND_MOVE(s12) {
 
     var new_move_index = game.chess.history().length;
     
-    var move_info = game.chess.move(s12.move_note_short);
+    var move_info = game.chess.move(s12.move_note_short, {sloppy:true});
     if (move_info) {
         //game.movetimes[new_move_index] = ficsobj.s12.move_time;
         game.fens[new_move_index] = game.chess.fen().split(' ')[0];
@@ -150,7 +149,7 @@ function appendMove(game_num, i) {
     if (game.last_move_highlighted) {
         $('.move_'+game_num).removeClass('highlight');
         move_div.addClass('highlight');
-        game.board.position(game.fens[i]);
+        game.board.position(game.fens[i],false);
         game.current_move_index = i;
     }
 }
@@ -211,16 +210,43 @@ function renderGame(game_num) {
     observe_div.appendTo($('#games_div'));
 
     renderPlayersDOM(game_num);
-    game.board = new ChessBoard('board_' + game_num, {position: game.chess.fen()});
+    game.board = new ChessBoard('board_' + game_num, {
+        position: game.chess.fen(),
+        onDrop : function(source, target, piece, newPos, oldPos, orientation) {
+            var mresult = game.chess.move({ from: source, to: target });
+            console.log(mresult);
+            if (!mresult) {
+                console.log(this);
+                return 'snapback';
+            }
+            game.fens[game.chess.history().length - 1] = game.chess.fen().split(' ')[0];
+            appendMove(game_num, game.chess.history().length-1);
+            //game.current_move_index = game.chess.history().length - 1;
+            //{ color: 'b', from: 'g8', to: 'e7', flags: 'n', piece: 'n', san: 'Ne7' }
+            arrows_game_num = game_num;
+            console.log("Source: " + source);
+            console.log("Target: " + target);
+            console.log("Piece: " + piece);
+            console.log("New position: " + ChessBoard.objToFen(newPos));
+            console.log("Old position: " + ChessBoard.objToFen(oldPos));
+            console.log("Orientation: " + orientation);
+            console.log("--------------------");
+            console.log('qwqweqweqweqweqwe' + game.game_num + arrows_game_num);
+        },
+        draggable:true
+    });
+    console.log(game.board.draggable);
+    game.board.draggable = false;
+    console.log(game.board.draggable);
 }
 
 
 
 
+var arrows_game_num = '';
 
 $(document).ready(function(){
     INIT();
-    var arrows_game_num = '';
 
     $(document).on('click', function(e) { 
         var observe_div = $(e.target).closest('[id^=observe_]');
@@ -232,6 +258,7 @@ $(document).ready(function(){
     });
     
     $(document).on('keydown', function(e) {
+        console.log(arrows_game_num + ' is arrows_game_num');
         if (!arrows_game_num) return;
 
         if ( $.inArray(e.which, [37,38,39,40]) == -1 ) return;
@@ -242,9 +269,13 @@ $(document).ready(function(){
         if (!game) return;
 
         if (e.which == 37) {            // left
-            goToMove(arrows_game_num, game.current_move_index - 1);
+            if (game.current_move_index > 0) {
+                goToMove(arrows_game_num, game.current_move_index - 1);
+            }
         } else if (e.which == 39) {     //right
-            goToMove(arrows_game_num, game.current_move_index + 1);
+            if (game.current_move_index < game.chess.history().length - 1) {
+                goToMove(arrows_game_num, game.current_move_index + 1);
+            }
         } else if (e.which == 38) {     //up
             goToMove(arrows_game_num, 0);
         } else if (e.which == 40) {     //down
